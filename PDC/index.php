fddc1f945fd13541c440f721f6bbd3ca0dd06f3e
pichucho013 <61350129+pichucho013@users.noreply.github.com>
2020-09-16 20:00:06 -0400

    Traduccion del index
    traduccion y ejemplos para presentar al cliente

--- PDC/index.php
+++ PDC/index.php
@@ -51,7 +51,7 @@
                         <a href="pdc/pdcmundo.php">PDC en el mundo</a>
                       </div>
                     </div>
-                    <a href="#news">Estatuto</a>
+                    <a href="pdc/estatuto.php">Estatuto</a>
                     <div class="dropdown">
                       <button class="dropbtn">Electoral 
                         <i class="fa fa-caret-down"></i>
@@ -95,8 +95,7 @@
                         <h3 class="medium text-primary">With Us</h3>
                       </div>
                       <h1 class="slider-header" data-caption-animate="fadeInUp" data-caption-delay="150">Partido Democrata Cristiano Paraguay</h1>
-                      <p class="text-bigger slider-text" data-caption-animate="fadeInUp" data-caption-delay="250">Strategies of our attorneys will help you solve very complex legal issues.</p>
-                      <div class="button-block" data-caption-animate="fadeInUp" data-caption-delay="400"><a class="button button-lg button-primary-outline-v2" href="#">Request a Free Consultation</a></div>
+                      <p class="text-bigger slider-text" data-caption-animate="fadeInUp" data-caption-delay="250">Texto del Slogan del partdido PDC .</p>
                     </div>
                   </div>
                 </div>
@@ -108,7 +107,7 @@
                   <div class="row justify-content-md-center">
                     <div class="col-md-11 col-lg-10 col-xl-9">
                       <div class="header-decorated" data-caption-animate="fadeInUp" data-caption-delay="0s">
-                        <h3 class="medium text-primary">We Offer</h3>
+                        <h3 class="medium text-primary">We Off</h3>
                       </div>
                       <h2 class="slider-header" data-caption-animate="fadeInUp" data-caption-delay="150">Affordable and Effective Legal Help</h2>
                       <p class="text-bigger slider-text" data-caption-animate="fadeInUp" data-caption-delay="250">Our expert team of attorneys and consultants will be glad to provide necessary legal assistance.</p>
@@ -156,7 +155,7 @@
                 </div>
                 <div class="divider bg-accent"></div>
                 <div class="box-body">
-                  <p>Business law deals with the creation of new businesses and the issues that arise as existing.</p>
+                  <p>Texto relacionado a la propuesta del area de la salud, clara, concisa y transparente.(no tan largo)</p>
                 </div>
               </article>
             </div>
@@ -170,7 +169,7 @@
                 </div>
                 <div class="divider bg-accent"></div>
                 <div class="box-body">
-                  <p>Family law attorneys help their clients file for separation or divorce, alimony or child custody.</p>
+                  <p>FTexto relacionado a la propuesta del area de la economia, clara, concisa y transparente.</p>
                 </div>
               </article>
             </div>
@@ -184,7 +183,7 @@
                 </div>
                 <div class="divider bg-accent"></div>
                 <div class="box-body">
-                  <p>Civil litigation is the process in which civil matters are resolved in a court of law.</p>
+                  <p>Texto relacionado a la propuesta del area de la Educación, clara, concisa y transparente.</p>
                 </div>
               </article>
             </div>
@@ -227,13 +226,13 @@
                   <figure class="thumbnail-image"><img src="images/team-9-246x300.jpg" alt="" width="246" height="300"/>
                   </figure>
                   <div class="thumbnail-inner">
-                    <div class="link-group"><span class="novi-icon icon icon-xxs icon-primary material-icons-local_phone"></span><a class="link-white" href="tel:#">+1 (409) 987–5874</a></div>
-                    <div class="link-group"><span class="novi-icon icon icon-xxs icon-primary fa-envelope-o"></span><a class="link-white" href="mailto:#">info@demolink.org</a></div>
+                    <div class="link-group"><span class="novi-icon icon icon-xxs icon-primary material-icons-local_phone"></span><a class="link-white" href="tel:#">+595 982–384 255</a></div>
+                    <div class="link-group"><span class="novi-icon icon icon-xxs icon-primary fa-envelope-o"></span><a class="link-white" href="mailto:#">info@ejemplo.org</a></div>
                   </div>
                   <div class="thumbnail-caption">
-                    <p class="text-header"><a href="#">Amanda Smith</a></p>
+                    <p class="text-header"><a href="#">Maria Laura</a></p>
                     <div class="divider divider-md bg-teak"></div>
-                    <p class="text-caption">Paralegal</p>
+                    <p class="text-caption">Viceprecidenta</p>
                   </div>
                 </div>
               </div>
@@ -244,13 +243,13 @@
                   <figure class="thumbnail-image"><img src="images/team-10-246x300.jpg" alt="" width="246" height="300"/>
                   </figure>
                   <div class="thumbnail-inner">
-                    <div class="link-group"><span class="novi-icon icon icon-xxs icon-primary material-icons-local_phone"></span><a class="link-white" href="tel:#">+1 (409) 987–5874</a></div>
-                    <div class="link-group"><span class="novi-icon icon icon-xxs icon-primary fa-envelope-o"></span><a class="link-white" href="mailto:#">info@demolink.org</a></div>
+                    <div class="link-group"><span class="novi-icon icon icon-xxs icon-primary material-icons-local_phone"></span><a class="link-white" href="tel:#">+595 982–384 255</a></div>
+                    <div class="link-group"><span class="novi-icon icon icon-xxs icon-primary fa-envelope-o"></span><a class="link-white" href="mailto:#">info@ejemplo.org</a></div>
                   </div>
                   <div class="thumbnail-caption">
-                    <p class="text-header"><a href="#">John Doe</a></p>
+                    <p class="text-header"><a href="#">Juan Perez</a></p>
                     <div class="divider divider-md bg-teak"></div>
-                    <p class="text-caption">Attorney</p>
+                    <p class="text-caption">Tesorero</p>
                   </div>
                 </div>
               </div>
@@ -261,13 +260,13 @@
                   <figure class="thumbnail-image"><img src="images/team-11-246x300.jpg" alt="" width="246" height="300"/>
                   </figure>
                   <div class="thumbnail-inner">
-                    <div class="link-group"><span class="novi-icon icon icon-xxs icon-primary material-icons-local_phone"></span><a class="link-white" href="tel:#">+1 (409) 987–5874</a></div>
-                    <div class="link-group"><span class="novi-icon icon icon-xxs icon-primary fa-envelope-o"></span><a class="link-white" href="mailto:#">info@demolink.org</a></div>
+                    <div class="link-group"><span class="novi-icon icon icon-xxs icon-primary material-icons-local_phone"></span><a class="link-white" href="tel:#">+595 982–384 255</a></div>
+                    <div class="link-group"><span class="novi-icon icon icon-xxs icon-primary fa-envelope-o"></span><a class="link-white" href="mailto:#">info@ejemplo.org</a></div>
                   </div>
                   <div class="thumbnail-caption">
-                    <p class="text-header"><a href="#">Vanessa Ives</a></p>
+                    <p class="text-header"><a href="#">Luis Miguel</a></p>
                     <div class="divider divider-md bg-teak"></div>
-                    <p class="text-caption">Legal Assistant</p>
+                    <p class="text-caption">Recursos humanos</p>
                   </div>
                 </div>
               </div>
@@ -277,7 +276,7 @@
                 <div class="block-number">06</div>
                 <h3 class="text-normal">Autoridades</h3>
                 <p class="h5 h5-smaller text-style-4">del PDC</p>
-                <p>If you or your business is facing a legal challenge, contact us today to arrange a free initial consultation with an attorney.</p><a class="link link-group link-group-animated link-bold link-secondary" href="#"><span>Read more</span><span class="novi-icon icon icon-xxs icon-primary fa fa-angle-right"></span></a>
+                <p>Texto relacionado a la seccion de Autoridades con el fin de introducir a la dicha seccion(clara concisa).</p><a class="link link-group link-group-animated link-bold link-secondary" href="#"><span>Read more</span><span class="novi-icon icon icon-xxs icon-primary fa fa-angle-right"></span></a>
               </div>
             </div>
           </div>
@@ -291,26 +290,6 @@
               <div class="row row-40">
                 <div class="col-sm-6 col-md-3">
                   <div class="box-counter box-counter-inverse"><span class="novi-icon icon icon-lg icon-primary mercury-icon-group"></span>
-                    <div class="text-large counter">1450</div>
-                    <p class="box-header">Happy Clients</p>
-                  </div>
-                </div>
-                <div class="col-sm-6 col-md-3">
-                  <div class="box-counter box-counter-inverse"><span class="novi-icon icon icon-lg-smaller icon-primary mercury-icon-scales"></span>
-                    <div class="text-large counter">23</div>
-                    <p class="box-header">Years of Experience</p>
-                  </div>
-                </div>
-                <div class="col-sm-6 col-md-3">
-                  <div class="box-counter box-counter-inverse"><span class="novi-icon icon icon-lg-smaller icon-primary mercury-icon-partners"></span>
-                    <div class="text-large counter counter-percent">98</div>
-                    <p class="box-header">Successful Cases</p>
-                  </div>
-                </div>
-                <div class="col-sm-6 col-md-3">
-                  <div class="box-counter box-counter-inverse"><span class="novi-icon icon icon-lg icon-primary mercury-icon-case"></span>
-                    <div class="text-large counter">7500</div>
-                    <p class="box-header">Personal Injury Cases</p>
                   </div>
                 </div>
               </div>
@@ -319,86 +298,23 @@
         </div>
       </section>
 
-
-      <section class="section-66 section-md-90 section-xl-bottom-100">
-        <div class="container">
-          <h3 class="text-center">Testimonials</h3>
-          <div class="owl-carousel owl-spacing-1 owl-nav-classic owl-style-minimal" data-autoplay="true" data-items="1" data-md-items="2" data-stage-padding="0" data-loop="true" data-margin="30" data-mouse-drag="true" data-nav="true" data-dots="true" data-dots-each="1">
-            <div class="item">
-              <blockquote class="quote-bordered">
-                <div class="quote-body">
-                  <div class="quote-open">
-                    <svg version="1.1" baseprofile="tiny" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="37px" height="27px" viewbox="0 0 21 15" preserveAspectRatio="none">
-                      <path d="M9.597,10.412c0,1.306-0.473,2.399-1.418,3.277c-0.944,0.876-2.06,1.316-3.349,1.316                    c-1.287,0-2.414-0.44-3.382-1.316C0.482,12.811,0,11.758,0,10.535c0-1.226,0.58-2.716,1.739-4.473L5.603,0H9.34L6.956,6.37                    C8.716,7.145,9.597,8.493,9.597,10.412z M20.987,10.412c0,1.306-0.473,2.399-1.418,3.277c-0.944,0.876-2.06,1.316-3.35,1.316                    c-1.288,0-2.415-0.44-3.381-1.316c-0.966-0.879-1.45-1.931-1.45-3.154c0-1.226,0.582-2.716,1.74-4.473L16.994,0h3.734l-2.382,6.37                    C20.106,7.145,20.987,8.493,20.987,10.412z"></path>
-                    </svg>
-                  </div>
-                  <div class="quote-body-inner">
-                    <h6>One of The Most Ethical Lawyers I’ve Ever Known...</h6>
-                    <p>
-                      <q>I’ve never known another lawyer more prepared or focused.  John has the ability to dissect a case much like a skilled surgeon, with absolute calm.  Nothing seems to distract him from his goal on a case, which of course, makes him extremely tough to beat.</q>
-                    </p>
-                  </div>
-                </div>
-                <div class="quote-footer">
-                  <div class="unit unit-horizontal unit-spacing-sm align-items-center">
-                    <div class="unit-left"><img class="img-circle" src="images/clients-testimonials-1-68x68.jpg" alt="" width="68" height="68"/>
-                    </div>
-                    <div class="unit-body">
-                      <cite>Mark Wilson</cite>
-                      <p class="text-primary">CFO, Wilson Ltd.</p>
-                    </div>
-                  </div>
-                </div>
-              </blockquote>
-            </div>
-            <div class="item">
-              <blockquote class="quote-bordered">
-                <div class="quote-body">
-                  <div class="quote-open">
-                    <svg version="1.1" baseprofile="tiny" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="37px" height="27px" viewbox="0 0 21 15" preserveAspectRatio="none">
-                      <path d="M9.597,10.412c0,1.306-0.473,2.399-1.418,3.277c-0.944,0.876-2.06,1.316-3.349,1.316                    c-1.287,0-2.414-0.44-3.382-1.316C0.482,12.811,0,11.758,0,10.535c0-1.226,0.58-2.716,1.739-4.473L5.603,0H9.34L6.956,6.37                    C8.716,7.145,9.597,8.493,9.597,10.412z M20.987,10.412c0,1.306-0.473,2.399-1.418,3.277c-0.944,0.876-2.06,1.316-3.35,1.316                    c-1.288,0-2.415-0.44-3.381-1.316c-0.966-0.879-1.45-1.931-1.45-3.154c0-1.226,0.582-2.716,1.74-4.473L16.994,0h3.734l-2.382,6.37                    C20.106,7.145,20.987,8.493,20.987,10.412z"></path>
-                    </svg>
-                  </div>
-                  <div class="quote-body-inner">
-                    <h6>LawExpert is One of The Best...</h6>
-                    <p>
-                      <q>John Doe is one of those attorneys who has it all-talent and skill, compassion for his clients, and the ability to communicate well with anyone he meets. This is one of the best combinations for a trial attorney and he is one of the best.</q>
-                    </p>
-                  </div>
-                </div>
-                <div class="quote-footer">
-                  <div class="unit unit-horizontal unit-spacing-sm align-items-center">
-                    <div class="unit-left"><img class="img-circle" src="images/clients-testimonials-2-68x68.jpg" alt="" width="68" height="68"/>
-                    </div>
-                    <div class="unit-body">
-                      <cite>Amanda Eberson</cite>
-                      <p class="text-primary">CEO, Eberson Co.</p>
-                    </div>
-                  </div>
-                </div>
-              </blockquote>
-            </div>
-          </div>
-        </div>
-      </section>
-
       
       <section class="section-50 section-md-75 section-xl-100">
         <div class="container">
-          <h3 class="text-center">Latest News</h3>
+          <h3 class="text-center">Nuevas Noticias</h3>
           <div class="row row-40 row-offset-1 justify-content-sm-center justify-content-md-start">
             <div class="col-sm-9 col-md-6 col-lg-4 col-xl-3">
               <article class="post-boxed">
                 <div class="post-boxed-image"><img src="images/home-5-268x182.jpg" alt="" width="268" height="182"/>
                 </div>
                 <div class="post-boxed-body">
-                  <div class="post-boxed-title"><a href="#">The Lawyers Collective Women's Rights Initiative</a></div>
+                  <div class="post-boxed-title"><a href="#">Nuevo virus llamo Corona Virus, las naciones estan sorprendidas.</a></div>
                   <div class="post-boxed-footer">
                     <ul class="post-boxed-meta">
                       <li>
-                        <time datetime="2019-06-14">JUNE 14, 2019</time>
+                        <time datetime="2019-06-14">MARZO 14, 2020</time>
                       </li>
-                      <li><span>by</span><a href="#">Admin</a></li>
+                      <li><span>Por</span><a href="#">ABC, color</a></li>
                     </ul>
                   </div>
                 </div>
@@ -409,13 +325,13 @@
                 <div class="post-boxed-image"><img src="images/home-6-268x182.jpg" alt="" width="268" height="182"/>
                 </div>
                 <div class="post-boxed-body">
-                  <div class="post-boxed-title"><a href="#">Legal Documents Every Landlord Needs</a></div>
+                  <div class="post-boxed-title"><a href="#">Primer caso de Corona Virus en Paraguay</a></div>
                   <div class="post-boxed-footer">
                     <ul class="post-boxed-meta">
                       <li>
-                        <time datetime="2019-06-20">JUNE 20, 2019</time>
+                        <time datetime="2019-06-20">JUNIO 20, 2019</time>
                       </li>
-                      <li><span>by</span><a href="#">Admin</a></li>
+                      <li><span>Por</span><a href="#">Admin</a></li>
                     </ul>
                   </div>
                 </div>
@@ -426,13 +342,13 @@
                 <div class="post-boxed-image"><img src="images/home-7-268x182.jpg" alt="" width="268" height="182"/>
                 </div>
                 <div class="post-boxed-body">
-                  <div class="post-boxed-title"><a href="#">Help Us Make the Law Accessible for Everyone</a></div>
+                  <div class="post-boxed-title"><a href="#">EPP ataca de nuevo, ciudadanos sorprendidos</a></div>
                   <div class="post-boxed-footer">
                     <ul class="post-boxed-meta">
                       <li>
-                        <time datetime="2019-06-23">JUNE 23, 2019</time>
+                        <time datetime="2019-06-23">JUNIO 23, 2019</time>
                       </li>
-                      <li><span>by</span><a href="#">Admin</a></li>
+                      <li><span>Por</span><a href="#">Admin</a></li>
                     </ul>
                   </div>
                 </div>
@@ -443,11 +359,11 @@
                 <div class="post-boxed-image"><img src="images/home-8-268x182.jpg" alt="" width="268" height="182"/>
                 </div>
                 <div class="post-boxed-body">
-                  <div class="post-boxed-title"><a href="#">5 Legal Documents for Military Service People</a></div>
+                  <div class="post-boxed-title"><a href="#">Primer caso de Corona Virus en Paraguay</a></div>
                   <div class="post-boxed-footer">
                     <ul class="post-boxed-meta">
                       <li>
-                        <time datetime="2019-06-12">JUNE 12, 2019</time>
+                        <time datetime="2019-06-12">jUNIO 12, 2019</time>
                       </li>
                       <li><span>by</span><a href="#">Admin</a></li>
                     </ul>
---
+++ PDC/pdc/estatuto.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html class="wide wow-animation" lang="en">
+  <head>
+    <title>PDC en el mundo</title>
+    <meta name="format-detection" content="telephone=no">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="utf-8">
+    <link rel="icon" href="../images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="../css/fuentes.css">
+    <link rel="stylesheet" href="../css/fonts.css">
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <!--[if lt IE 10]>
+    <div style="background: #212121; padding: 10px 0; box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3); clear: both; text-align:center; position: relative; z-index:1;"><a href="http://windows.microsoft.com/en-US/internet-explorer/"><img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
+    <script src="js/html5shiv.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="preloader">
+      <div class="preloader-body">
+        <div class="cssload-container">
+          <div class="cssload-speeding-wheel"> </div>
+        </div>
+        <p>Cargando...</p>
+      </div>
+    </div>
+    <div class="page">
+      <header class="page-head">
+        
+        <div class="rd-navbar-wrap">
+          <nav class="rd-navbar rd-navbar-default" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-md-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-fixed" data-lg-device-layout="rd-navbar-fixed" data-xl-layout="rd-navbar-static" data-xl-device-layout="rd-navbar-static" data-xxl-layout="rd-navbar-static" data-xxl-device-layout="rd-navbar-static" data-lg-stick-up-offset="53px" data-xl-stick-up-offset="53px" data-xxl-stick-up-offset="53px" data-lg-stick-up="true" data-xl-stick-up="true" data-xxl-stick-up="true">
+            <div class="rd-navbar-inner">
+
+            <?php require('../view/header.php'); ?>
+
+              <div class="rd-navbar-group">
+                <div class="rd-navbar-panel">
+                  <a class="rd-navbar-brand brand" href="../index.php"><img src="../images/logo.png" alt="" width="100px"/></a>
+                </div>
+                <div class="topnav" id="myTopnav">
+                    <a href="../index.php">Inicio</a>
+                    <div class="dropdown">
+                      <button class="dropbtn" class="activo">Nosotros 
+                        <i class="fa fa-caret-down"></i>
+                      </button>
+                      <div class="dropdown-content">
+                        <a href="../pdc/quienesomos.php">Quienes somos</a>
+                        <a href="#">Autoridades</a>
+                        <a href="#">Organigrama</a>
+                        <a href="#">PDC en el mundo</a>
+                      </div>
+                    </div>
+                    <a href="#news">Estatuto</a>
+                    <div class="dropdown">
+                      <button class="dropbtn">Electoral 
+                        <i class="fa fa-caret-down"></i>
+                      </button>
+                      <div class="dropdown-content">
+                        <a href="#">Sala Electoral</a>
+                        <a href="#">Candidaturas</a>
+                      </div>
+                    </div> 
+                    <div class="dropdown">
+                      <button class="dropbtn">Prensa 
+                        <i class="fa fa-caret-down"></i>
+                      </button>
+                      <div class="dropdown-content">
+                        <a href="#">Noticias</a>
+                        <a href="#">Actividades</a>
+                        <a href="#">Galeria</a>
+                        <a href="#">Documentos</a>
+                      </div>
+                    </div> 
+                    <a href="../contactos.php">Contacto</a>
+                    <a href="#" style="height: 57px;" class="icon" onclick="myFunction()">&#9776;</a>
+                  </div>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </header>
+
+      <section class="section-30 section-md-40 section-lg-66 section-xl-bottom-90 bg-gray-dark page-title-wrap" style="background-image: url(../images/banner-quienes.png);">
+        <div class="container">
+          <div class="page-title">
+            <h2>Estatuto del PDC</h2>
+          </div>
+        </div>
+      </section>
+
+    <iframe src="estatuto.pdf" style="width:500px; height:375px;" frameborder="0"></iframe>
+
+      <?php require('../view/footer.php'); ?>
+    <div class="snackbars" id="form-output-global"></div>
+    <script src="../js/core.min.js"></script>
+    <script src="../js/script.js"></script>
+  </body>
+</html>
